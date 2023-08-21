3d8351e2a8ade0bf03bb4128512c02173a5a151e
Kinuseka <realkingseeker1089@gmail.com>
2023-08-21 22:45:07 +0800

    Update updater.py

--- essentials/updater.py
+++ essentials/updater.py
@@ -7,7 +7,6 @@ import json
 import os
 import sys
 import time
-import subprocess
 import zipfile
 import io
 import shutil
@@ -23,7 +22,7 @@ class UpdateInformation:
     Current_Version: list = [0,6,1] 
     Version: list = None
     Message: str = None
-    Additional: dict = None
+    Additional: dict = {}
     #Library Versioning
     A_MaV = 0
     A_MiV = 0
@@ -36,17 +35,20 @@ class UpdateInformation:
 def init():
     try:
         req = requests.get(UpdateInformation.Version_Host)
-        data = req.json()[UpdateInformation.Name_ver]
-        version = data['Version']
-        if data.get('Message', None):
-            UpdateInformation.Message = data['Message']
-        if data.get('Additional', None):
-            UpdateInformation.Additional = data['Additional']
-        if data.get('Version', None):
-            UpdateInformation.Version = data['Version']
+        sort_data(req)
+        if UpdateInformation.Additional.get('New_Host', None):
+            UpdateInformation.Version_Host = UpdateInformation.Additional['New_Host']
+            return init()
         UpdateInformation.initialized = True
     except Exception as e:
         UpdateInformation.init_error = e
+
+def sort_data(req: requests.Response):
+    data: dict = req.json()[UpdateInformation.Name_ver]
+    version = data['Version']
+    UpdateInformation.Message = data.get('Message', None)
+    UpdateInformation.Additional = data.get('Additional', {})
+    UpdateInformation.Version = data.get('Version', None)
 
 def Version():
     return UpdateInformation.Version
@@ -54,7 +56,7 @@ def Version():
 def CurrentVersion():
     return UpdateInformation.Current_Version
 
-def ConstructVerion(Version: list):
+def ConstructVersion(Version: list):
     return f"{Version[0]}.{Version[1]}.{Version[2]}"
 
 def Comparator(version, remote):
@@ -79,10 +81,10 @@ def show_update(logger, loggon):
         return
     new_update, future_update = Comparator(UpdateInformation.Current_Version, UpdateInformation.Version)
     if new_update:
-        logger.info(f"New update found: v{ConstructVerion(Version())}")
+        logger.info(f"New update found: v{ConstructVersion(Version())}")
     elif future_update:
         logger.warn("This version is from the future, might be unstable.")
-        logger.info(f"Available: v{ConstructVerion(Version())} | Your version: v{ConstructVerion(CurrentVersion())}")
+        logger.info(f"Available: v{ConstructVersion(Version())} | Your version: v{ConstructVersion(CurrentVersion())}")
     message = UpdateInformation.Message
     if all((new_update, message)) or all((future_update, message)):
         logger.info(f'[Announce]: {message}')
@@ -93,36 +95,7 @@ def _new_update():
     if n: return True
     return False
 
-def git_method(logger=None):
-    def _sub_processor(process):
-        for line, errl in zip(process.stdout, process.stderr):
-            print(line.strip(), errl.strip())
-            if logger:
-                logger.info(line.strip())
-                logger.info(errl.strip())
-    def _result_judge(process):
-        process.wait()
-        if process.returncode == 0:
-            # Git command executed successfully
-            return True, None
-        else:
-            return False, process.stderr
-    properdir = Path(__main__.__file__).parent.resolve()
-    process = subprocess.Popen(['git','pull'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, cwd=properdir)
-    _sub_processor(process)
-    result, err = _result_judge(process)
-    if not result:
-        return False
-    #if success then proceed to step 2
-    version_str = ConstructVerion(Version())
-    process = subprocess.Popen(['git','checkout',version_str], stdout=subprocess.PIPE, stderr=subprocess.PIPE,text=True, cwd=properdir)
-    _sub_processor(process)
-    result, err = _result_judge(process)
-    print(err)
-    if not result:
-        return False
-    return True #update success
-def md5_for_file(f, block_size=2**20):
+def md5_for_file(f: io.BufferedReader, block_size=2**20):
     md5 = hashlib.md5()
     while True:
         data = f.read(block_size)
@@ -130,6 +103,7 @@ def md5_for_file(f, block_size=2**20):
             break
         md5.update(data)
     return md5.digest()
+
 def merge(path: Path, into: Path):
     """Copies path to into, recursing into children (if path is a directory) and ignoring already existing files."""
     if not path.exists():
@@ -149,9 +123,10 @@ def merge(path: Path, into: Path):
                 shutil.copy2(path, into)
             return
         shutil.copy2(path, into)
+
 def zip_method(logger=None):
     print("Using zip method")
-    version_str = ConstructVerion(Version())
+    version_str = ConstructVersion(Version())
     api_url = f'https://api.github.com/repos/Kinuseka/nScraper/releases/tags/{version_str}'
     try:
         response = requests.get(api_url)
@@ -174,22 +149,6 @@ def zip_method(logger=None):
                     updatedPath = Path(PurePath(croot,cdir[0]))
                     merge(updatedPath, properdir)
                     break
-                    # for iroot, idir, ifile in os.walk(os.path.join(croot,cdir[0])):
-                    #     print(idir, iroot, ifile)
-                    #     #This part should be inside the folder
-                    #     for roots, dirs, files in os.walk(idir[0]):
-                    #         for file in files:
-                    #             direct = PurePath(roots, file)
-                    #             dest = PurePath(temp_folder, direct.name)
-                    #             print("Checking: ", direct, dest)
-                    #             shutil.move(direct, dest)
-                    #         for dir in dirs:
-                    #             direct = PurePath(roots, dir)
-                    #             dest = PurePath(temp_folder, direct.name)
-                    #             print("Checking: ", direct, dest)
-                    #             merge(Path(direct), Path(dest))
-                    #     break
-                    # break
             shutil.rmtree(temp_folder)
             print("Update Successful")
             return True
@@ -206,16 +165,8 @@ def zip_method(logger=None):
 def upgrade(logger=None):
     n, f = Comparator(UpdateInformation.Current_Version, UpdateInformation.Version)
     if n: 
-        # status = git_method(logger)
         status = zip_method(logger)
         if not status:
-            # status = zip_method(logger)
-            # status = False
-            # if status:
-            #     return True
-            # else:
-            #     print("Update was not successful")
-            #     return False
             print("Update was not successful")
             return False
         else:
